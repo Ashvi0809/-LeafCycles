94af9c8a4039185ba2d974c3566c91d6ac66f703
Vidhi Patel <vidhipatel254731@gmail.com>
2025-07-20 16:25:34 -0400

    Cart

--- plantsapp/views.py
+++ plantsapp/views.py
@@ -15,6 +15,8 @@ from .models import WishlistItem
 from .forms import ProductForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from .models import CartItem
+
 
 
 from .models import Product
@@ -179,3 +181,23 @@ def wishlist_view(request):
     wishlist_items = WishlistItem.objects.filter(user=request.user)
     return render(request, 'plantsapp/wishlist.html', {'wishlist_items': wishlist_items})
 
+@csrf_exempt
+def add_to_cart(request, product_id):
+    if request.method == 'POST' and request.user.is_authenticated:
+        product = Product.objects.get(id=product_id)
+        cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
+        return JsonResponse({'status': 'added'})
+    return JsonResponse({'status': 'unauthorized'}, status=403)
+
+
+@login_required
+def cart_view(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'plantsapp/cart.html', {
+        'cart_items': cart_items,
+        'total_price': total_price
+    })
