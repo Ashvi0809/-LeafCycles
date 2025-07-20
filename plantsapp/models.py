096fc2f4c7801225bfe1c3861ae738582e5c4961
Vidhi Patel <vidhipatel254731@gmail.com>
2025-07-20 14:47:43 -0400

    Wishlist

--- plantsapp/models.py
+++ plantsapp/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, name, number, pincode, password=None):
@@ -33,13 +35,28 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
    # image = models.ImageField(upload_to='products/')
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
    # uploaded_at = models.DateTimeField(auto_now_add=True)
   #  uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
+
+
+class WishlistItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return f"{self.user.email} - {self.product.name}"
